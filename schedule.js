713ca85969c0ea5a0a87448630f583b012ee9322
Giga <gigabyte5671@gmail.com>
2022-10-29 21:51:22 +1300

    Update scheduling stuff

--- schedule.js
+++ schedule.js
@@ -1,9 +1,21 @@
 var schedule = [
 	{
+		title: 'Sleep',
+		colour: 'red',
+		start: '00:00',
+		end: '09:00'
+	},
+	{
 		title: 'Work',
 		colour: 'green',
 		start: '10:00',
-		end: '20:00'
+		end: '16:00'
+	},
+	{
+		title: 'Lunch',
+		colour: 'green',
+		start: '12:00',
+		end: '13:00'
 	},
 	{
 		title: 'Meetings',
@@ -21,6 +33,12 @@ var schedule = [
 		title: 'Work',
 		colour: 'green',
 		start: '17:00',
-		end: '20:00'
+		end: '19:00'
+	},
+	{
+		title: 'Sleep',
+		colour: 'red',
+		start: '22:00',
+		end: '24:00'
 	}
 ];
